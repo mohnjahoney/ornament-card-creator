d3d16bef9bcd5e76ee824671df1d6925cc20dd26
John Mahoney <appleseed@Johns-MacBook-Air-2.local>
2026-03-02 21:51:41 -0800

    standalone animation

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
+import HelloTest from "./pages/HelloTest";
+
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -19,6 +21,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/hello" element={<HelloTest />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         {/* </HashRouter> */}
